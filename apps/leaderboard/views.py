d4fb04516a7158966573825fd13d48716bf8c757
ChristeenaJestin <christeenajestin7@gmail.com>
2026-04-13 09:42:27 +0530

    initial backend setup

--- apps/leaderboard/views.py
+++ apps/leaderboard/views.py
@@ -5,5 +5,7 @@ from apps.users.models import User
 class LeaderboardView(APIView):
     def get(self, request):
         users = User.objects.order_by('-points')[:10]
-        data = [{"username": u.username, "points": u.points} for u in users]
-        return Response(data)
+        return Response([
+            {"username": u.username, "points": u.points}
+            for u in users
+        ])
