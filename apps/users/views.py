be1cf1493109da6c892f9e514b6b16030306f563
ChristeenaJestin <christeenajestin7@gmail.com>
2026-04-19 10:38:45 +0530

    updated

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,6 +4,9 @@ from rest_framework import status
 from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from rest_framework.permissions import IsAuthenticated
+
+
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
@@ -25,3 +28,13 @@ class LoginView(APIView):
                 "refresh": str(refresh)
             })
         return Response(serializer.errors, status=400)
+    # users/views.py
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "username": request.user.username,
+            "email": request.user.email,
+        })
