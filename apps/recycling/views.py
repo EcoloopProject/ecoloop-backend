c9a9e23bab3dde0d0892c2cfe4b4c8098c4cf964
ChristeenaJestin <christeenajestin7@gmail.com>
2026-05-30 11:52:50 +0530

    backend updated

--- apps/recycling/views.py
+++ apps/recycling/views.py
@@ -7,9 +7,19 @@ class SubmitRecycling(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+
+        quantity = int(request.data['quantity'])
+
         Recycling.objects.create(
             user=request.user,
             waste_type=request.data['waste_type'],
-            quantity=request.data['quantity']
+            quantity=quantity
         )
-        return Response({"message": "Submitted"})
+
+        request.user.points += quantity * 10
+        request.user.save()
+
+        return Response({
+            "message": "Submitted",
+            "points": request.user.points
+        })
